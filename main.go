146ddceb6f89d52098598f9b64297771864cd947
DAlba-sudo <d.alba.work@gmail.com>
2024-10-19 15:44:21 -0400

    improved logging

--- main.go
+++ main.go
@@ -4,19 +4,24 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/thejerf/suture/v4"
 )
 
 var (
-	ctx = context.Background()
+	ctx         = context.Background()
+	InfoLogger  = log.New(os.Stdout, "[Info] ", log.LstdFlags)
+	DebugLogger = log.New(os.Stdout, "[Debug] ", log.LstdFlags)
+	ErrorLogger = log.New(os.Stdout, "[Error] ", log.LstdFlags)
 )
 
 func main() {
 	port := flag.Int("port", 8443, "port to use when binding to the server")
 	flag.Parse()
 	main_supervisor := suture.NewSimple("WebServer")
-	
+
 	ws := WebServer{
 		address: "0.0.0.0",
 		port:    *port,
--- server.go
+++ server.go
@@ -16,9 +16,9 @@ type WebServer struct {
 func (ws WebServer) Serve(ctx context.Context) error {
 	// generate the address, port pair
 	address := fmt.Sprintf("%s:%d", ws.address, ws.port)
-
-	http.Handle("/published/", http.StripPrefix("/published", http.FileServer(http.Dir("./blog/published/"))))
 	http.Handle("/blog/", http.StripPrefix("/blog", blog.GetMux()))
+
+	DebugLogger.Printf("Binding to %s...", address)
 
 	// serve the server
 	err := http.ListenAndServe(address, nil)
